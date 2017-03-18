baad655d0be277799b40d84b758a934d39c6c8bf
eao197 <eao197@gmail.com>
2017-03-18 12:00:16 +0300

    Update for outliving_reference_t.

--- dev/cpp_util_3/outliving.hpp
+++ dev/cpp_util_3/outliving.hpp
@@ -98,7 +98,7 @@ public :
 	explicit outliving_reference_t(T & r) : base_type(r) {}
 
 	template<typename U>
-	outliving_reference_t(U && r) : base_type(r) {}
+	explicit outliving_reference_t(U && r) : base_type(r) {}
 
 	outliving_reference_t(T &&) = delete;
 	outliving_reference_t(outliving_reference_t const & o)
